25d1fc5de948a6ae1ad19050ee6514e0b3466b7a
shimitar <8bi2sz6723@gmail.com>
2022-09-13 15:46:35 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def new
+  end
+
+ def create
+  @room = Room.find(params[:room_id])
+  # 特定のツイートに紐づくインスタンスを生成し、属性値を指定
+  @message = @room.messages.new(message_params)
+  # インスタンスを保存
+
+  if @message.save
+    redirect_to room_messages_path(@room)
+  else
+    render :index
+   end
+ end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
